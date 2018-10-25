46ee2545c4fc664357714f1b645f85a06c6cdbae
Ryan Oldford <ryan.oldford@gmail.com>
2018-10-25 15:21:16 +0800

    Updates

---
+++ precache-manifest.48cca06216ed71a97a918d78125da327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7908af31e3d631d9746",
+    "url": "/change-for-change/static/css/main.4e8bf927.chunk.css"
+  },
+  {
+    "revision": "d7908af31e3d631d9746",
+    "url": "/change-for-change/static/js/main.d7908af3.chunk.js"
+  },
+  {
+    "revision": "cdea558ebfb418344ea6",
+    "url": "/change-for-change/static/js/1.cdea558e.chunk.js"
+  },
+  {
+    "revision": "df06dc62cc0a67258f4f",
+    "url": "/change-for-change/static/js/runtime~main.df06dc62.js"
+  },
+  {
+    "revision": "9add4843051087ad88e5b78e17a62206",
+    "url": "/change-for-change/index.html"
+  }
+];
