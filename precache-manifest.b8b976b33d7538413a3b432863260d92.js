d9e2b2b6460ccf202cd373e35e033f018eb31588
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-09 04:12:42 +0000

    Deploy ROldford/change-for-change to github.com/ROldford/change-for-change.git:gh-pages

---
+++ precache-manifest.b8b976b33d7538413a3b432863260d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d5a8a8b09ee16a5cecc",
+    "url": "/change-for-change/static/css/main.37aaae86.chunk.css"
+  },
+  {
+    "revision": "4d5a8a8b09ee16a5cecc",
+    "url": "/change-for-change/static/js/main.4d5a8a8b.chunk.js"
+  },
+  {
+    "revision": "099184adcfa4b35a4f3d",
+    "url": "/change-for-change/static/css/1.e5406bb2.chunk.css"
+  },
+  {
+    "revision": "099184adcfa4b35a4f3d",
+    "url": "/change-for-change/static/js/1.099184ad.chunk.js"
+  },
+  {
+    "revision": "df06dc62cc0a67258f4f",
+    "url": "/change-for-change/static/js/runtime~main.df06dc62.js"
+  },
+  {
+    "revision": "5d765ed3ab170e177235f7c8841b8d25",
+    "url": "/change-for-change/index.html"
+  }
+];
