9a5705c994f98936f72c2bbbdc321331d8415625
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-23 15:53:17 +0800

    Updated test to check that cards are divided up into decks properly

--- src/DynamicCardDeckManager.test.js
+++ src/DynamicCardDeckManager.test.js
@@ -1,7 +1,7 @@
 import DynamicCardDeckManager from "./DynamicCardDeckManager";
 
-const setup = () => {
-  return [
+const setup = (cardsNeeded) => {
+  let cards = [
     {
       key: 1,
       data: {
@@ -93,16 +93,69 @@ const setup = () => {
       }
     }
   ];
+  if (cardsNeeded>=cards.length) {
+    return cards;
+  } else {
+    return cards.slice(0, cardsNeeded);
+  }
 }
 
 describe('Tests', () => {
   it("renders correctly", () => {
-    let cards = setup();
+    let cards = setup(10);
     const wrapper = shallow(
       <DynamicCardDeckManager cards={cards} deckSize={5} />
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  const testDeckDivision = (totalCards, deckSize) => {
+    let numberOfDecks = Math.ceil(totalCards/deckSize);
+    let fullDecks = Math.floor(totalCards/deckSize);
+    let cards = setup(totalCards);
+    const wrapper = shallow(
+      <DynamicCardDeckManager cards={cards} deckSize={deckSize} />
+    );
+    const foundDecks = wrapper.find('DynamicCardDeck');
+    expect(foundDecks.length).toEqual(numberOfDecks);
+    let deckSizes = [];
+    foundDecks.forEach((deck) => {
+      deckSizes.push(deck.props().cards.length);
+    });
+    expect(deckSizes.length).toEqual(numberOfDecks);
+    expect(deckSizes.filter((x) => {return x == deckSize;}).length).toEqual(fullDecks);
+    expect(deckSizes.reduce((acc, curr) => {return acc + curr;})).toEqual(totalCards);
+  }
+
+  // renders enough decks for all cards, all decks have same size
+  it("renders enough decks when cards divide evenly", () => {
+    testDeckDivision(10, 2);
+    testDeckDivision(9, 3);
+    testDeckDivision(8, 4);
+    testDeckDivision(8, 2);
+    testDeckDivision(6, 3);
+    testDeckDivision(6, 2);
+    testDeckDivision(5, 5);
+  })
+
+  it("renders enough decks when cards do not divide evenly", () => {
+    testDeckDivision(10, 4);
+    testDeckDivision(10, 3);
+    testDeckDivision(9, 5);
+    testDeckDivision(9, 4);
+    testDeckDivision(9, 2);
+    testDeckDivision(8, 5);
+    testDeckDivision(8, 3);
+    testDeckDivision(7, 5);
+    testDeckDivision(7, 4);
+    testDeckDivision(7, 3);
+    testDeckDivision(7, 2);
+    testDeckDivision(6, 5);
+    testDeckDivision(6, 4);
+    testDeckDivision(5, 4);
+    testDeckDivision(5, 3);
+    testDeckDivision(5, 2);
+  })
 
   // it("renders blank cards when needed", () => {
   //   let cards = setup();
