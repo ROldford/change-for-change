9711eb7e4d62517547a3b3132f5ebff3d7b1cfe6
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-23 08:47:45 +0800

    Fixed CustomCard imports and styling

--- src/CustomCard.js
+++ src/CustomCard.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import {Card, CardImg, CardBody, CardTitle, CardText} from 'reactstrap';
+import {Card, CardBody, CardTitle, CardText} from 'reactstrap';
 import Image from 'react-graceful-image';
 
 let CustomCard = (props) => {
   const data = props.data;
-  const imgStyle = {
-    display: 'block',
-    maxWidth: 64,
-    width: 'auto',
-    height: 'auto',
-    margin: 'auto'
-  }
   return (
     <Card className="m-2">
       <CardBody>
