1ba2b699c2b73d13b6faea45e5ae26f1fd800a39
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-23 10:24:10 +0800

    Fixed setup for prop type testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow, render, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { createSerializer } from "enzyme-to-json";
 import sinon from "sinon";
+import 'jest-prop-type-error';
 
 // Set the default serializer for Jest to be the from enzyme-to-json
 // This produces an easier to read (for humans) serialized format.
