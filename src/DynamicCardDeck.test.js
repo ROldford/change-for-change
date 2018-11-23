a656941bcd9671fbf7c5bd30d7f1cf2a7de565aa
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-23 14:30:45 +0800

    Tests now get testing input data properly

--- src/DynamicCardDeck.test.js
+++ src/DynamicCardDeck.test.js
@@ -1,39 +1,77 @@
 import DynamicCardDeck from "./DynamicCardDeck";
 
-const cards = [{
-  imageSource: 'https://www.countryflags.io/ca/flat/64.png',
-  imageAltText: 'Card image cap',
-  cardTitle: '1 Card title',
-  cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-},
-{
-  imageSource: 'https://www.countryflags.io/ca/flat/64.png',
-  imageAltText: 'Card image cap',
-  cardTitle: '2 Card title',
-  cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-},
-{
-  imageSource: 'https://www.countryflags.io/ca/flat/64.png',
-  imageAltText: 'Flag: Canada',
-  cardTitle: '3 Card title',
-  cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-}];
-
-it("renders correctly", () => {
-  const wrapper = shallow(
-    <DynamicCardDeck cards={cards} deckSize=3 />
-  );
-  expect(wrapper).toMatchSnapshot();
-});
-
-it("renders blank cards when needed", () => {
-  const wrapper = shallow(
-    <DynamicCardDeck cards={cards} deckSize=5 />
-  )
-  const images = wrapper.find('Image');
-  expect(images.length).toEqual(5);
-  const placeholder_images = images.filterWhere((image) => {
-    image.props().src === 'https://www.countryflags.io/aa/flat/64.png'
+describe('Tests', () => {
+  it("renders correctly", () => {
+    let cards = [{
+      key: "1",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Card image cap',
+        cardTitle: '1 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    },
+    {
+      key: "2",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Card image cap',
+        cardTitle: '2 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    },
+    {
+      key: "3",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Flag: Canada',
+        cardTitle: '3 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    }];
+    const wrapper = shallow(
+      <DynamicCardDeck cards={cards} deckSize={3} />
+    );
+    expect(wrapper).toMatchSnapshot();
   });
-  expect(placeholder_images.length).toEqual(2);
-});
+
+  it("renders blank cards when needed", () => {
+    let cards = [{
+      key: "1",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Card image cap',
+        cardTitle: '1 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    },
+    {
+      key: "2",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Card image cap',
+        cardTitle: '2 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    },
+    {
+      key: "3",
+      data: {
+        imageSource: 'https://www.countryflags.io/ca/flat/64.png',
+        imageAltText: 'Flag: Canada',
+        cardTitle: '3 Card title',
+        cardText: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+      }
+    }];
+    const wrapper = shallow(
+      <DynamicCardDeck cards={cards} deckSize={5} />
+    )
+    expect(wrapper).toMatchSnapshot();
+    const foundCards = wrapper.find('CustomCard');
+    expect(foundCards.length).toEqual(5);
+    // const placeholderCards = foundCards.filterWhere((card) => {
+    //   card.props().data.imageSource === 'https://www.countryflags.io/aa/flat/64.png'
+    // });
+    // expect(placeholderCards.length).toEqual(2);
+  });
+})
